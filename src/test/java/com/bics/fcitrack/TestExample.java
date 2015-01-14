5cbcff09bfab7a5719ef8def7d38026586b27de9
ShostakRV <morfiy.roma@gmail.com>
2015-01-14 21:06:59 +0200

    Test example

---
+++ src/test/java/com/bics/fcitrack/TestExample.java
@@ -0,0 +1,16 @@
+package com.bics.fcitrack;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by morfi_000
+ * Date: 14-Jan-15.
+ */
+public class TestExample {
+    @Test
+    public void someTest(){
+        Assert.assertTrue(true);
+    }
+}
