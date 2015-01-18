40d1e1f5ed855ce9e375904e7059eb400a0251e8
ShostakRV <morfiy.roma@gmail.com>
2015-01-18 22:59:56 +0200

    Main

--- src/main/java/com/bics/fcitrack/service/ProductServiceImpl.java
+++ src/main/java/com/bics/fcitrack/service/ProductServiceImpl.java
@@ -6,12 +6,14 @@ import com.bics.fcitrack.model.Product;
 import com.bics.fcitrack.service.interfaces.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by morfi_000
  * Date: 18-Jan-15.
  */
 @Component
+@Transactional
 public class ProductServiceImpl extends AbstractServiceImpl<Product> implements ProductService {
     @Autowired
     private ProductDao  productDao;
