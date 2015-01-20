462a12664423092bdd26bdd25dd2c210394ad7c5
ShostakRV <morfiy.roma@gmail.com>
2015-01-21 00:04:54 +0200

    test

--- src/main/java/com/bics/fcitrack/service/CommonConfigServiceImpl.java
+++ src/main/java/com/bics/fcitrack/service/CommonConfigServiceImpl.java
@@ -3,20 +3,21 @@ package com.bics.fcitrack.service;
 import com.bics.fcitrack.dao.AbstractDao;
 import com.bics.fcitrack.dao.CommonConfigDao;
 import com.bics.fcitrack.model.CommonConfig;
-import com.bics.fcitrack.model.Product;
 import com.bics.fcitrack.service.interfaces.CommonConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Created by godex_000 on 20.01.2015.
+ * Created by godex_000
+ * Date: 20.01.2015.
  */
 @Service("commonConfigService")
 @Transactional
 public class CommonConfigServiceImpl extends AbstractServiceImpl<CommonConfig> implements CommonConfigService {
     @Autowired
     private CommonConfigDao commonConfigDao;
+
     @Override
     protected AbstractDao<CommonConfig> getDao() {
         return commonConfigDao;
