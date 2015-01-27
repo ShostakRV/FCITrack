cce390891f379907f3bb96d02962d93bd67cd3b3
ShostakRV <morfiy.roma@gmail.com>
2015-01-27 21:45:29 +0200

    -----

--- src/main/java/com/bics/fcitrack/web/managedbeans/AbstractBeanImpl.java
+++ src/main/java/com/bics/fcitrack/web/managedbeans/AbstractBeanImpl.java
@@ -4,7 +4,7 @@ import com.bics.fcitrack.dao.AbstractDao;
 import com.bics.fcitrack.dao.hibernate.AbstractDaoImpl;
 import com.bics.fcitrack.service.AbstractServiceImpl;
 import com.bics.fcitrack.service.interfaces.AbstractService;
-import com.bics.fcitrack.web.managedbeans.interfaces.AbstractBean;
+
 
 import java.io.Serializable;
 
@@ -12,20 +12,24 @@ import java.io.Serializable;
  * Created by godex_000
  * on 27.01.2015.
  */
-public abstract class AbstractBeanImpl<T> implements AbstractBean<T> {
-    public AbstractServiceImpl service = null;
-    @Override
+public abstract class AbstractBeanImpl<T> {
+
+
     public void create(T t) {
     }
 
-    @Override
+
     public void save(T t) {
 
     }
 
-    @Override
+
     public void delete(T t) {
 
     }
+
+    public abstract AbstractService getService();
+
+
 }
 
